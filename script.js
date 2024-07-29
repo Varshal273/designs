47b7ee9962abcc204d9270a72ec8b4362b8719f1
Varshal <jadvanivarshal237@gmail.com>
2024-07-29 22:18:07 +0530

    header modified

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 let bg = document.getElementById('background');
-for(let i=0;i<10*100;i++){
+for(let i=0;i<5*100;i++){
     bg.appendChild(document.createElement("div"));
 }
+let hd = document.getElementById('header');
+let hd_str = [
+    {'str':'Welcome!'},
+    {'str':'hello world!'},
+    {'str':'Web Designer'},
+    {'str':'Web Developer!'}
+];
+let count = 0;
+setInterval(() => {
+    hd.children[0].innerText = hd_str[count].str;
+    count++;
+    if(count == hd_str.length){ count = 0;}
+}, 1000);
