6af6d95141eb62f6d7124f45ded5a329ef64af45
Varshal <jadvanivarshal237@gmail.com>
2024-07-30 23:17:23 +0530

    added about me and re-designed

--- script.js
+++ script.js
@@ -2,16 +2,16 @@ let bg = document.getElementById('background');
 for(let i=0;i<10*100;i++){
     bg.appendChild(document.createElement("div"));
 }
-let hd = document.getElementById('header');
-let hd_str = [
-    {'str':'Welcome!'},
-    {'str':'Hello World!'},
-    {'str':'Web Designer'},
-    {'str':'Web Developer!'}
-];
-let count = 0;
-setInterval(() => {
-    hd.children[0].innerText = hd_str[count].str;
-    count++;
-    if(count == hd_str.length){ count = 0;}
-}, 3000);
+// let hd = document.getElementById('header');
+// let hd_str = [
+//     {'str':'Welcome!'},
+//     {'str':'Hello World!'},
+//     {'str':'Web Designer'},
+//     {'str':'Web Developer!'}
+// ];
+// let count = 0;
+// setInterval(() => {
+//     hd.children[0].innerText = hd_str[count].str;
+//     count++;
+//     if(count == hd_str.length){ count = 0;}
+// }, 3000);
